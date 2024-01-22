27504a6efc1b3ba45d63ea2f80ee8a9451ce2a22
Viktoras Kondrotas <viktoras.kondrotas@gmail.com>
2024-01-22 23:03:19 +0200

    fetched listings and single listing

--- helpers.php
+++ helpers.php
@@ -17,12 +17,13 @@ function basePath($path = ''){
  * @return void
  * 
  */
-function loadView($name) {
+function loadView($name, $data = []) {
 
     $viewPath = basePath("views/{$name}.view.php");
     
 
     if(file_exists($viewPath)) {
+        extract($data);
         require $viewPath;
     } else {
         echo "View '{$name} not found!'";
@@ -69,3 +70,14 @@ function inspectAndDie($value) {
     die(var_dump($value));
     echo '</pre>';
 }
+
+/**
+ * Format salary
+ * 
+ * @param string $salary
+ * @return string Formatted Salary
+ */
+
+ function formatSalary($salary) {
+    return '$' . number_format(floatval($salary));
+ }
