dd8284a82806612b6cb73bc573bad0ac6954564a
Viktoras Kondrotas <viktoras.kondrotas@gmail.com>
2024-01-24 21:53:07 +0200

    Validation Class, Form Submission & Sanitizing Data

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -2,6 +2,7 @@
 namespace App\Controllers;
 
 use Framework\Database;
+use Framework\Validation;
 
 class ListingController {
     protected $db;
@@ -19,6 +20,7 @@ class ListingController {
  */
 
     public function index(){
+        
         $listings = $this->db->query('SELECT * FROM listings')->fetchAll();
 
         loadView('listings/index', ['listings' => $listings]);
@@ -59,5 +61,22 @@ class ListingController {
         loadView('listings/show', [
             'listing' => $listing
         ]);
+    }
+    /**
+     * Store data in database
+     * 
+     * @return void
+     * 
+     */
+    
+     public function store()
+     {
+        $allowedfields = ['title', 'description', 'salary', 'tags', 'company', 'address', 'city', 'state', 'phone', 'email', 'requirements', 'benefits'];
+     
+        $newListingData = array_intersect_key($_POST, array_flip($allowedfields));
+
+        $newListingData['user_id'] = 1;
+
+        $newListingData = array_map('sanitize', $newListingData);
     }
 }
