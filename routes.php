b86322213c3e71bb51fbd7e1b28f444acddbf7df
Viktoras Kondrotas <viktoras.kondrotas@gmail.com>
2024-01-31 11:47:00 +0200

    Register validation, Register User, Session class

--- routes.php
+++ routes.php
@@ -10,4 +10,7 @@ $router->post('/listings', 'ListingController@store');
 $router->put('/listings/{id}', 'ListingController@update');
 $router->delete('/listings/{id}', 'ListingController@destroy');
 
+$router->get('/auth/register', 'UserController@create');
+$router->get('/auth/login', 'UserController@login');
 
+$router->post('/auth/register', 'UserController@store');
